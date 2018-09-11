0df8a3c33a8ad293467624fe4203d95853224b4d
Gia1987 <giacomo.n27@gmail.com>
2018-09-11 19:40:49 +0100

    Remove todo implemented

--- src/App.js
+++ src/App.js
@@ -30,7 +30,9 @@ class App extends Component {
   }
 
   removeTodo(id){
-    console.log("removing: ", id);
+    this.setState({
+      todos: this.state.todos.filter((todo, index) => todo.id !== id)
+    });
   }
   render() {
     return (
