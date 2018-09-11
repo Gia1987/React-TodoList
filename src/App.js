c346aba08e65347f546f5c4e32680dd93838a16b
Gia1987 <giacomo.n27@gmail.com>
2018-09-11 17:00:52 +0100

    Header component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Header from './components/header';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div  className='todo-wrapper'>
-          hello
+          <Header/>
         </div>
       </div>
     );
