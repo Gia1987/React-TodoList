20999397b13a102659ab87e73d800d475cb0e544
Gia1987 <giacomo.n27@gmail.com>
2018-09-11 16:51:30 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div  className='todo-wrapper'>
+          hello
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
